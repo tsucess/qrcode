03c8a9432cb93c13f47fb5290a259052e2e1c0f7
OGUNSANYA TAOFEEQ ABIDEMI <taofeeq.muhammad22@gmail.com>
2022-09-08 08:24:52 +0100

    uploaded

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const container = document.querySelector(".container"),
+qrInput = container.querySelector(".form input"),
+generatorBtn = container.querySelector(".form button");
+qrImage = container.querySelector(".qr-code img");
+
+
+
+generatorBtn.addEventListener("click", () =>{
+    let qrValue = qrInput.value;
+    if(!qrValue) return;
+    qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`;
+
+    container.classList.add("active");
+})
