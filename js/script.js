b727b5a8960c894f7263cf5cdfc0c920f339dd7c
OGUNSANYA TAOFEEQ ABIDEMI <taofeeq.muhammad22@gmail.com>
2022-09-09 06:20:44 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ generatorBtn = container.querySelector(".form button");
 qrImage = container.querySelector(".qr-code img");
 
 
+// default qr Code;
+let qrValue = "create your qrcode";
+qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`;
+
 
 generatorBtn.addEventListener("click", () =>{
     let qrValue = qrInput.value;
